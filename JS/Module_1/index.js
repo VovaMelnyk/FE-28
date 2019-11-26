f28eda35e0d980462faa591474d18eb3991419bd
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2019-11-26 21:56:48 +0200

    add module 1

---
+++ JS/Module_1/index.js
@@ -0,0 +1,444 @@
+"use strict";
+
+// var email = "user@gmail.com";
+// let password = "sdjflsdjlsj";
+// const age = 34;
+// let carPrice = 400;
+// const PRICE = 12;
+
+//  ;jadf;dfjd
+/*
+skdf;sdkfsdfksdk;flskd
+lsjdflsdjflsdjflsdjflj
+dfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf
+sdfsdfsdfsdfsdfsd
+*/
+
+// const number = 4;
+// const string = "Hello World 4";
+// // const bool = true
+// const bool = false;
+// const n = null;
+// const u = undefined;
+
+// const name = "Americano";
+// const price = 25;
+// const currency = "UAH";
+// const size = "medium";
+// const isMilk = true;
+
+// const americano = {
+//   name: "Americano",
+//   price: 25,
+//   currency: "UAH",
+//   size: "medium",
+//   isMilk: true
+// };
+
+// const tea = {
+//   name: "Tea",
+//   price: 12,
+//   currency: "UAH",
+//   size: "large",
+//   isMilk: false
+// };
+
+// const nameTea = "Tea";
+// const priceTea = 12;
+// const currencyTea = "UAH";
+// const sizeTea = "large";
+// const isMilkTea = false;
+
+// typeof
+// const n = 3;
+// const s = "Hello";
+// const b = false;
+
+// console.log("Check variable n ", typeof n);
+// console.log("Check variable s", typeof s);
+// console.log("Check variable b", typeof b);
+
+// console.log(tea);
+// console.log(typeof w);
+// console.log(typeof num);
+// console.log(typeof b);
+// console.log(typeof un);
+// console.log(typeof empty);
+// console.log(typeof coffe);
+
+// let answer = prompt('Enter your name');
+// console.log(answer);
+// let answer2 = confirm('Are you happy?');
+// console.log(answer2);
+
+// Create variables
+// console.log(x);
+// // console.log(y);
+// // console.log(z);
+// var x = 5;
+// let y = 10;
+// const z = 50;
+
+// const
+
+// var x = 5;
+// let y = 10;
+// const z = 50;
+// x = 20;
+// console.log(x);
+// y = 30;
+// console.log(y);
+// z = 60;
+// console.log(z);
+
+// duplicate declared
+// var x = 5;
+// var x = 10;
+// console.log(x);
+
+// let y = 20;
+// let y = 30;
+// console.log(y); // Error
+
+// const z = 50;
+// const z = 60;
+// console.log(z); // Error
+
+// if(true) {
+//     var a = 10;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// Hoisting
+
+// console.log(x); // undefined
+// var x;
+// x = 5;
+// var x = 5;
+
+// console.log(y); // is not defined
+// let y = 10;
+// console.log('Hello');
+
+// console.log(z); // is not defined
+// const z = 50;
+
+// +
+//
+
+// let a = 5;
+// // a = a + 1;
+// // a += 1;
+
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+// let a = 5;
+// let b = 10;
+// let result = a + b;
+// console.log(result);
+// console.log(typeof result);
+
+// let a = 6;
+// let b = false;
+// let c = "12";
+// let d = 1;
+// let result = a + d + c + b;
+// console.log(result);
+// console.log(typeof result);
+
+// let a = "Join ";
+// let b = "us";
+// console.log(a + b);
+// console.log(typeof a + b)
+
+// let a = 3;
+// let b = ' join';
+// console.log(a + b);
+
+// let a = true;
+// let b = " join";
+// console.log(a + b);
+
+// let a = true;
+// let b = 12;
+// console.log(a + b);
+
+// let a = true;
+// let b = true;
+// console.log(a + b);
+
+// let a = true;
+// let b = "false";
+// console.log(a + b);
+
+// let a = 12;
+// let b = null;
+// console.log(a + b);
+
+// let a = undefined;
+// let b = 10;
+// console.log(a + b);
+
+// let a = undefined;
+// let b = false;
+// console.log(a + b);
+// console.log(typeof NaN);
+// NEXT
+// -
+
+// let a = 10;
+// let b = 5;
+// console.log(b - a);
+
+// let a = 'join us';
+// let b = 'us';
+// let result = a - b;
+// console.log(result);
+// console.log(typeof result);
+
+// let a = 10;
+// let b = '2';
+// let c = 5;
+// console.log(a + b - c);
+
+// let a = 10;
+// let b = 'true';
+// console.log(a - b);
+
+// *
+
+// let a = 10;
+// let b = 5;
+// console.log(a * b);
+
+// let a = 2;
+// let b = 'start';
+// console.log(a * b);
+
+// let a = 2;
+// let b = '2.2';
+// console.log(a * b);
+
+// let a = 10;
+// let b = undefined;
+// console.log(a * b);
+
+// let a = 10;
+// let b = Infinity;
+// console.log(a / 0);
+
+//  %
+// let a = 10;
+// let b = 3;
+// console.log(a % b);
+
+// console.log('object');
+// alert('Hello');
+
+// let answer = prompt('How old are you');
+// console.log(answer);
+
+// let answer = confirm('Are you ready?');
+// console.log(answer);
+
+// let num1 = prompt('Enter number');
+// let num2 = prompt('Enter number');
+// let num3 = prompt('Enter number');
+// console.log(Number(num3));
+// console.log(Number(num1) + Number(num2) + Number(num3));
+
+// let a = '2rem';
+// let b = '150px';
+// let c = '2.5vw';
+// let d = 'qweqw25';
+
+// console.log(parseInt(a));
+// console.log(parseInt(b));
+// console.log(parseFloat(c));
+
+// let a = 0.1;
+// let b = 0.2;
+// let result = a + b
+// console.log(result.toFixed(2));
+
+// let a = 'Hello';
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.includes('he'));
+// console.log(a.indexOf('e'));
+// console.log(a.indexOf('ll'));
+
+// let name = 'Homer';
+// let lastName = 'Simpson';
+// let town = 'Springfield';
+
+// console.log(name + lastName + 'lives' + 'in' + town);
+// console.log(`${name} ${lastName} lives in ${town}`);
+
+// Logic and if
+// let a = 5;
+// let b = '5';
+// console.log(a == b);
+// console.log(a === b);
+
+//
+// let a = NaN,
+//     b = NaN;
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = 0,
+// b = null;
+// console.log(a == b);
+// console.log(a === b);
+//
+// let a = null,
+// b = null;
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = null,
+// b = undefined;
+
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = 0,
+// b = undefined;
+// console.log(a == b);
+// console.log(a === b);
+//
+// let a = 'a',
+//     b = null;
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = 'H';
+// let b = 'h';
+
+// console.log(a == b);
+// console.log(a === b);
+
+// let c = "Hello";
+// let d = "Hello";
+// console.log(c == d);
+// console.log(c === d);
+
+// let a = 5;
+// let b = 10;
+
+// console.log(a >= b);
+// let register = true;
+// let isPremium = false;
+
+// console.log(register && isPremium);
+// let login = 'user';
+// let email = 'dou@gmail.com';
+
+// let userData = 'dou@gmail.com';
+// console.log(userData === login || userData === email);
+// || or
+// && and
+// ! not
+
+// console.log('Отримав відпустку' && "Квитки" && 'Train');
+// console.log('Отримав відпустку' || "Квитки" || 'Train');
+
+// false
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(-0));
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(false));
+
+// true
+// console.log(Boolean('add'));
+// console.log(Boolean(Infinity));
+// console.log(Boolean(' '));
+// console.log(Boolean(-5));
+
+// console.log(0 || '' || '0');
+// console.log(0 || null || '0');
+// console.log(0 || undefined || null);
+// console.log(4 + 5 === 8 || 5 * 4 === '20' || 'q' + 'w' === 'w');
+// console.log(0 || '' || 0);
+// console.log(0 || null || 'undefined');
+// console.log(NaN || null || 5);
+// console.log(28 || null || '');
+// console.log(Infinity || 55 || NaN);
+// console.log(undefined || 'true' || NaN);
+
+// console.log('a' && 1 && 0);
+// console.log('a' && 1 && 2);
+// console.log('a' && 0 && 2);
+// console.log('0' && NaN && 2);
+// console.log('' && NaN && 2);
+// console.log(' ' && NaN && 2);
+// console.log({age:25} && 'null' && 2);
+// console.log({age:25} && null && 2);
+// console.log({age:25} && undefined && 2);
+// console.log({age:25} && 8 && -Infinity);
+// console.log(false && 'false' && -Infinity);
+// console.log('true' && -0 && null);
+
+// const correctLogin = 'admin';
+// const userLogin = prompt('Enter login');
+
+// if(userLogin !== correctLogin) {
+//     console.log('Error');
+// } else {
+//     console.log('Hello');
+// }
+
+// const userNumber = prompt('enter number 1-3');
+// let title = null;
+
+// if (Number(userNumber) === 1) {
+//     title = 'Error';
+// } else if (Number(userNumber) === 2) {
+//     title = 'Congratulation';
+// } else if (Number(userNumber) === 3) {
+//     title = 'Info';
+// } else {
+//     alert ('Wrong number');
+// }
+
+// switch (Number(userNumber)) {
+//     case 1:
+//         title = 'Error';
+//         break;
+//     case 2:
+//         title = 'Congratulation';
+//         break;
+//     case 3:
+//         title = 'Info';
+//         break;
+//     default:
+//         alert('Wrong number');
+//         break;
+// }
+
+// title = Number(userNumber) === 1 ? 'Error' : Number(userNumber) === 2 ? 'congratulation' : Number(userNumber) === 3 ? 'info' : alert('Wrong');
+
+// console.log(title);
+
+// const password = prompt('Enter password');
+
+// condition ? true : false
+// let result = null;
+// if(password.length >= 4) {
+//     result = 'correct';
+// } else {
+//     result = 'error';
+// }
+// result = password.length >= 4 || password ? 'correct' : 'error';
+
