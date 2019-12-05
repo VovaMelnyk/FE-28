1baf14c502cedf5277d6535563ad071041895b6e
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2019-12-05 22:05:15 +0200

    add module 2

---
+++ JS/Module_2/index.js
@@ -0,0 +1,211 @@
+// const password = prompt("Enter password");
+// if (password === "admin") {
+//   alert("Welcome admin");
+// } else if (password === "user") {
+//   alert("Welcome user");
+// } else if (password === "batman") {
+//   alert("Welcome batman");
+// } else {
+//   alert("Wrong password");
+// }
+
+// switch (password) {
+//   case "admin":
+//     alert("Welcome admin");
+//     break;
+//   case "user":
+//     alert("Welcome user");
+//     break;
+//   case "batman":
+//     alert("Welcome batman");
+//     break;
+//   default:
+//     alert("Wrong password");
+//     break;
+// }
+
+// const isDark = false;
+// let theme;
+
+// if (isDark) {
+//   theme = "dark";
+// } else {
+//   theme = "light";
+// }
+
+// let theme = isDark ? "dark" : "light";
+// let theme = isDark === true ? "dark" : isDark === null ? "light" : "blue";
+
+// 1) перевірка на роботу
+// 2) лічильник
+// 3) тіло циклу
+// 4) зміна лічильника
+
+// let count = 0;
+// while (count < 10) {
+//   console.log(count);
+//   count++;
+// }
+
+// let count = 0;
+// do {
+//   console.log(count);
+//   count++;
+// } while (count < 10);
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   //   if (i % 2 === 1) {
+//   //     continue;
+//   //   }
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// const emails = ["user@gmail.com", "tester@gmail.com", "admin@gmail.com"];
+// console.log(emails[1]);
+// console.log(emails[0]);
+// console.log(emails[5]);
+// console.log(emails.length); // 3
+// console.log(emails[-1]); // undefined
+// console.log(emails[emails.length - 1]);
+// emails.pop();
+// let lastElement = emails.pop();
+// console.log(lastElement);
+// console.log(emails);
+// emails.push("support@gmail.com");
+// console.log(emails);
+// let firstElement = emails.shift();
+// console.log(firstElement);
+// emails.unshift("manager@gmail.com");
+// console.log(emails);
+// emails.splice(2, 3); // delete
+// emails.splice(2, 1, "user2@gmail.com"); // replace
+// emails.splice(2, 0, "user2@gmail.com"); // add
+// console.log(emails);
+// const newArr = emails.slice();
+// console.log(emails.includes('tester@gmail.com'));
+
+// const checkEmail = 'tester@gmail.com';
+// for(let i = 0; i < emails.length; i++) {
+//     if(emails[i] === checkEmail) {
+//         console.log(true);
+//         break;
+//     }
+//     if( i === emails.length - 1) {
+//         console.log(false);
+//     }
+// }
+
+// for(let element of emails) {
+//     console.log(element);
+// }
+
+// console.log(emails.indexOf("tester@gmail.com"));
+
+// for (let el of emails) {
+//     if (el === "tester@gmail.com") {
+
+//     }
+// }
+
+// for (let i = 0, length = emails.length; i < length; i ++) {
+//     if (el === "tester@gmail.com") {
+//         console.log(i);
+//     }
+// }
+// const logins = ['user', 'tester', 'admin'];
+
+// const newArray = [].concat(emails,logins);
+
+// console.log(emails);
+// console.log(logins);
+// console.log(newArray);
+
+// const newEmails = emails.slice();
+// newEmails.reverse();
+// console.log('original',emails);
+// console.log('reverse', newEmails);
+
+// const str = "oko";
+// const str2 = "Hello";
+
+// const strSplit = str
+//   .split("")
+//   .reverse()
+//   .join("");
+// console.log(strSplit);
+// console.log(str === strSplit);
+
+// const str2Split = str2.split("");
+// const reverseArray = str2Split.reverse();
+// const reverseString = reverseArray.join("");
+// console.log(str2 === reverseString);
+
+// let a = 5;
+// let b = a;
+// console.log(a);
+// console.log(b);
+
+// a = 45;
+
+// console.log(a);
+// console.log(b);
+
+// const numbers = [1, 2, 4, 5, 6];
+// const newNumbers = numbers;
+// newNumbers = null;
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// numbers.push(10);
+
+// console.log(numbers);
+// console.log(newNumbers);
+// greet();
+
+// function greet() {
+//   alert("Hello");
+// }
+
+// const greet = function() {
+//   alert("Hello");
+// };
+// function checkLeapYear(year) {
+//   if (year % 4 === 0) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// let result = checkLeapYear(2004);
+// let result2 = checkLeapYear(2001);
+
+// console.log(result);
+// console.log(result2);
+
+const emails = ["user@gmail.com", "tester@gmail.com", "admin@gmail.com"];
+const anotherEmails = ["user2@gmail.com"];
+const userEmail = "admin@gmail.com";
+function checkEmailsExist(email, emailsArray) {
+  //   if (emailsArray.includes(email)) {
+  //     return true;
+  //   }
+  //   return false;
+  if (typeof email === "string") {
+    return emailsArray.includes(email);
+  }
+  return "Error";
+}
+
+const result = checkEmailsExist(userEmail, emails);
+const result2 = checkEmailsExist(userEmail, anotherEmails);
+
+console.log(result);
+console.log(result2);
