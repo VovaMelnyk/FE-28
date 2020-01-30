be4771499d2b83623c7ac328397e64020ef38fe0
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-01-30 17:09:00 +0200

    add task module 7

--- JS/Module_8/index.js
+++ JS/Module_8/index.js
@@ -76,12 +76,6 @@ function calculateProfit (e) {
 resetBtn.addEventListener('click', clearForm)
 form.addEventListener('submit', calculateProfit)
 
-
-
-
-
-
-
 // function playSound(e) {
 //   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 //   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
