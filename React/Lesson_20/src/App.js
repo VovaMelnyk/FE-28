497b2e3f713384a4545be51554d5263a2825f75f
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-06-03 20:56:41 +0300

    connect redux form

--- React/Lesson_20/src/App.js
+++ React/Lesson_20/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Form from "./Formik/Form";
+// import Form from "./Redux-form/Form";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ React/Lesson_20/src/Formik/Form.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const formState = { firstName: "", lastName: "", email: "", password: "" };
+
+const Registration = () => {
+  const [form, setForm] = useState(formState);
+
+  const inputHandler = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setForm({ ...form, [name]: value });
+  };
+
+  return (
+    <form>
+      <h3>Sign Up Formik</h3>
+
+      <div className="form-group">
+        <label>First name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          onChange={inputHandler}
+          name="firstName"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Last name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Last name"
+          onChange={inputHandler}
+          name="lastName"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          autoComplete="off"
+          onChange={inputHandler}
+          name="email"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          autoComplete="off"
+          onChange={inputHandler}
+          name="password"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary btn-block">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default Registration;
