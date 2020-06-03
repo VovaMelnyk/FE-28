1159b504b0412e9a7f7feab5162b1f1086828f99
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-06-03 20:40:19 +0300

    add form packages

--- React/Lesson_19/index.js
+++ React/Lesson_19/index.js
@@ -1,6 +1,91 @@
-// let exp = new RegExp('title');
-// let exp = /w/;
-// let str = 'Hello World';
+// let exp = new RegExp("World");
+// let exp = /World/ig;
+// let str = "Hello world world";
 
 // console.log(str.match(exp));
 // console.log(exp.test(str));
+
+// Classes
+
+// const age = "12";
+// const exp = /\d\d\d/g;
+// console.log(age.match(exp));
+// console.log(exp.test(age));
+
+// const age = "12";
+// const exp = /\D/g;
+// console.log(age.match(exp));
+// console.log(exp.test(age));
+
+// const name = "Bob 23";
+// const exp = /\w/g;
+
+// console.log(name.match(exp));
+// console.log(exp.test(name));
+
+// const name = "Bob23";
+// const exp = /\W/g;
+
+// console.log(name.match(exp));
+// console.log(exp.test(name));
+
+// const name = " Bob ";
+// const exp = /\s/g;
+
+// console.log(name.match(exp));
+// console.log(exp.test(name));
+
+// const name = " ";
+// const exp = /\S/g;
+
+// console.log(name.match(exp));
+// console.log(exp.test(name));
+
+// const age = "Bob age is  24 55.";
+// const exp = /\b\w\w\b/g;
+
+// console.log(age.match(exp));
+// console.log(exp.test(age));
+
+// const number = "054 87 96 654 3456";
+// const exp = /\b\d\d\d\b/g;
+// const exp = /\b\d{3,4}\b/g;
+// const exp = /\b\d{2}\b/g;
+// const exp = /\b\d{3,}\b/g;
+
+// console.log(number.match(exp));
+// console.log(exp.test(number));
+
+// const str = "Hello World a 123";
+// const exp = /\b[a-zA-Z]{2,}\b/g;
+// const exp = /\b[0-9]{2,}\b/g;
+// const exp = /\b\d{2,}\b/g;
+
+// console.log(str.match(exp));
+// console.log(exp.test(str));
+
+// const url = 'https';
+// const exp = /http(s)?/i
+
+// console.log(url.match(exp));
+// console.log(exp.test(url));
+
+// const number = "045 345 56 77 sdfsdlfkjl sdlfjsdlfjsdlf df";
+// const exp = /\b\d{1,}\b/g
+// const exp = /\b[0-9]{1,}\b/g;
+// const exp = /\b\d+\b/g;
+// const exp = /\b[0-9]+\b/g;
+
+// const exp = /\d*/g;
+
+// console.log(number.match(exp));
+// console.log(exp.test(number));
+
+// const login = "user";
+// const exp = /^[a-z]{4,16}$/;
+// const exp = /^.{4,16}$/;
+
+// const cardNumber = '4111111111111111';
+// const exp = /\d{4}/g;
+// console.log(cardNumber.match(exp));
+
