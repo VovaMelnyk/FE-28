3145f0a863491c4362f07afa84082f17fd47b247
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-06-09 16:50:20 +0300

    add ts progect

---
+++ React/Lesson_21/ts/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Basic from "./Basic";
+
+function App() {
+  return <Basic />;
+}
+
+export default App;
---
+++ React/Lesson_21/ts/src/Basic.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Basic = () => {
+  return (
+    <div>
+      <h1>BASIC</h1>
+    </div>
+  );
+};
+
+export default Basic;
